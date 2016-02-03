ad4b4e4e4d4b062fb43791467288d603a24aec95
eeikonl <kenneth.oneill@ericsson.com>
2016-02-03 14:37:39 +0000

    Changed first test to use list of Strings

--- src/main/java/ie/dacelonid/CsvParser.java
+++ src/main/java/ie/dacelonid/CsvParser.java
@@ -1,13 +1,18 @@
 package ie.dacelonid;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CsvParser {
 	private static final String DELIMITER = "\t";
 
-	public List<String> parseCsv(String testCSV) {
-		return Arrays.asList(testCSV.split("\\s*"+ DELIMITER +"\\s*"));
+	public List<List<String>> parseCsv(List<String> testCSV) {
+		List<List<String>> parsedStrings = new ArrayList<>();
+		for(String line: testCSV){
+			parsedStrings.add(Arrays.asList(line.split("\\s*"+ DELIMITER +"\\s*")));
+		}
+		return parsedStrings;
 	}
 
 }
--- src/test/java/ie/dacelonid/CsvParserTest.java
+++ src/test/java/ie/dacelonid/CsvParserTest.java
@@ -3,6 +3,7 @@ package ie.dacelonid;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,14 +11,20 @@ import org.junit.Test;
 
 public class CsvParserTest {
 
+
 	@Test
-	public void verifyParsing_singleLine_getCorrectValues() {
+	public void parseCsv_multiLine_getCorrectValues() {
 		String testCSV="test1\ttest2\ttest3";
+		List<String> inputs = new ArrayList<>();
 		CsvParser objUnderTest = new CsvParser();
-		List<String> expectedValues =  Arrays.asList(testCSV.split("\\s*\t\\s*"));
-		List<String> actualValues = objUnderTest.parseCsv(testCSV);
+		List<List<String>> expectedValues = new ArrayList<>();
+		for(int x = 0;x < 10;x++){
+			inputs.add(testCSV);
+			expectedValues.add(Arrays.asList(testCSV.split("\\s*\t\\s*")));
+		}
+		List<List<String>> actualValues = objUnderTest.parseCsv(inputs);
 		
 		assertThat(actualValues, is(expectedValues));
 	}
-
+	
 }
